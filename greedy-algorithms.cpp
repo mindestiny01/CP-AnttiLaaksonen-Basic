4c0395b69763eb2b8e74826e95e31a3da0e0cc56
SAMUEL PANDOHAN TERAMPIL GULTOM <samuel.gultom@student.president.ac.id>
2022-08-13 12:34:24 +0700

    Intitial commit

---
+++ greedy-algorithms.cpp
@@ -0,0 +1,48 @@
+/*
+    Created by: mindestiny19
+    Time: 11 August 2022  19:07:09
+
+    compile: g++ -std=c++17 -O2 -Wall <file-name>.cpp -o <folder-name>/<file-name>.exe
+*/
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define PB push_back
+#define MP make_pair
+#define F first
+#define S second
+#define FOR(i, a, b) for (int i = a; i <= b; i++)
+#define SQ(a) (a) * (a)
+
+
+typedef vector<int> vi;
+typedef pair<int, int> pi;
+typedef long long ll;
+typedef long long int int64;
+typedef long int int32;
+
+int epsilon = 1e-9;
+int a, b, d, e, m, n, k[epsilon];
+
+void solve(){
+  
+}
+
+int main(){
+  ios::sync_with_stdio(0); cin.tie(0);
+  
+  //freeopen("input.txt", "r", stdin);
+  //freeopen("output.txt", "r", stdout);
+
+  int i = 1; int tt;
+  cin >> tt;
+  while (tt--){
+    cout << "Case #"<<i<<" : ";
+    solve();
+    i++;
+  }
+
+  return 0;
+}
---
+++ time-complexity.cpp
@@ -0,0 +1,55 @@
+/*
+    Created by: mindestiny19
+    Time: 11 August 2022  19:07:09
+
+*/
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define PB push_back
+#define MP make_pair
+#define F first
+#define S second
+#define FOR(i, a, b) for (int i = a; i <= b; i++)
+#define SQ(a) (a) * (a)
+
+
+typedef vector<int> vi;
+typedef pair<int, int> pi;
+typedef long long ll;
+typedef long long int int64;
+typedef long int int32;
+
+int a, b, d, e, m, n, k[1000000];
+
+void solve(){
+  // Check README.md for explanations.
+  cout<<"Time Complexity!"<<endl;
+  cin >> a >> b;
+  string intro = ""; string nope = ""; string tell = "";
+  
+  // Calculation Notes
+  // O(...) -> represent of time complexity for some functions
+
+}
+
+
+
+int main(){
+  ios::sync_with_stdio(0); cin.tie(0);
+  
+  //freeopen("input.txt", "r", stdin);
+  //freeopen("output.txt", "r", stdout);
+
+  int i = 1; int tt;
+  cin >> tt;
+  while (tt--){
+    cout << "Case #"<<i<<" : ";
+    solve();
+    i++;
+  }
+
+  return 0;
+}
